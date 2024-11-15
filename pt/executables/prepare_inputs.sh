834c937b58ddaa5c3888df9a74bdc707ab045304
Karan Vahi <vahi@isi.edu>
2024-11-15 17:07:08 -0500

    updates to the notebook.

--- pt/executables/prepare_inputs.sh
+++ pt/executables/prepare_inputs.sh
@@ -5,13 +5,14 @@ set -e
 TOP_DIR=`pwd`
 
 
-echo "Cloning the modelzoo repository into ./input"
+echo "Going to rsynch the modelzoo repository into ./input"
 mkdir -p input
 cd input
 
 rm -rf ./modelzoo
 rsync -PaL /ocean/neocortex/cerebras/modelzoo ./
 chmod u+w -R ./modelzoo
+echo "rsynch of the modelzoo repository into ./input completed."
 
 # remove the params.yaml file as we track it as a separate input to the wf
 echo "Removing params.yaml from pytorch config dir"
