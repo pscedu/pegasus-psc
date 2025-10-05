359e7442e449f55c77a79a00684b47d2aa64c742
julian <julianx@gmail.com>
2025-10-05 11:33:24 -0400

    Adjusting path to use the untarred contents instead of the existing Ocean location.

--- psc-workflow/executables/step2/run_regression.sh
+++ psc-workflow/executables/step2/run_regression.sh
@@ -8,4 +8,4 @@ tar xvzf merged_dataset_ocelot.tgz
 tar xvzf pretrain_OCELOT__pretrain_MS_0_0001__model_pretrain.tgz
 
 # TODO: Run the "create_regression_csv.py" and "run_roberta.py" steps before running this one. The dirs mentioned in the YAML require on that step finishing.
-python3 run_regression.py --mode train --is_pretrained_checkpoint --checkpoint_path ${ENTRY_LOCATION}/pretrain_OCELOT/pretrain_MS_0.0001/model_pretrain/checkpoint_3000.mdl --params ${ENTRY_LOCATION}/regression_params.yaml --model_dir ${ENTRY_LOCATION}/regression_OCELOT/ms_OCELOT
+python3 run_regression.py --mode train --is_pretrained_checkpoint --checkpoint_path pretrain_OCELOT/pretrain_MS_0.0001/model_pretrain/checkpoint_3000.mdl --params ${ENTRY_LOCATION}/regression_params.yaml --model_dir ${ENTRY_LOCATION}/regression_OCELOT/ms_OCELOT
