0ba3f07bf07767633ecf4ede7690b0ad95041da2
julian <julianx@gmail.com>
2025-10-05 11:34:18 -0400

    Setting tar to use a single file instead of the whole checkpoints directory, as the current step is taking almost an hour just transferring files.

--- psc-workflow/executables/step1/run_roberta.sh
+++ psc-workflow/executables/step1/run_roberta.sh
@@ -8,6 +8,6 @@ tar xvzf csv_val.tgz
 
 # TODO: This substep will run on Neocortex
 #run the pre-training on RoBERTa
-python-pt run_roberta.py --mode train --cs_ip ${CS_IP_ADDR} --params ${ENTRY_LOCATION}/roberta_params_OCELOT_MS.yaml --model_dir ${ENTRY_LOCATION}/pretrain_OCELOT/pretrain_MS_0.0001/model_pretrain
+python-pt run_roberta.py --mode train --cs_ip ${CS_IP_ADDR} --params ${ENTRY_LOCATION}/roberta_params_OCELOT_MS.yaml --model_dir ${ENTRY_LOCATION}/pretrain_OCELOT/pretrain_MS_0.0001/model_pretrain/
 
-tar cvzf pretrain_OCELOT__pretrain_MS_0_0001__model_pretrain.tgz --directory=${ENTRY_LOCATION}/ pretrain_OCELOT/pretrain_MS_0.0001/model_pretrain
+tar cvzf pretrain_OCELOT__pretrain_MS_0_0001__model_pretrain.tgz --directory=${ENTRY_LOCATION}/ pretrain_OCELOT/pretrain_MS_0.0001/model_pretrain/checkpoint_3000.mdl
